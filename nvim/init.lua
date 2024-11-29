d9091ea4e1be01dc93e23f48e1443e34dfc94107
naricon <lub137ss@gmail.com>
2024-11-29 03:25:23 -0300

    lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,2 @@
---vim.cmd.colorscheme 'habamax'
---vim.cmd.colorscheme 'murphy'
-vim.cmd.colorscheme 'elflord'
---vim.cmd.colorscheme 'ron'
---vim.cmd.colorscheme 'vim'
-
-vim.g.mapleader = ' '
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.clipboard = 'unnamedplus'
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.scrolloff = 15
-
---Keymaps
-
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+require("config.lazy")
+require("config.remap")
