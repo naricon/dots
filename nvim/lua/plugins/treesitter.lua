c8b8dd2c1dd42c007fa1ad9d28c741c84b76ee62
naricon <lub137ss@gmail.com>
2024-11-29 03:34:00 -0300

    auto_install

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,8 +2,8 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python" },
-			autoinstall = true,
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "java" },
+			auto_install = true,
 			highlight = {
 				enable = true,
 			}
