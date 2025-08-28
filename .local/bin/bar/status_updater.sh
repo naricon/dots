fa285c3966d3a93f64460b57de59719293c04866
naricon <lub137ss@gmail.com>
2025-08-27 21:26:11 -0400

    jlhawd

---
+++ .local/bin/bar/status_updater.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+for pid in $(pidof -x "status_updater.sh"); do
+    if [ $pid != $$ ]; then
+        echo "$(date +"%F %T"): status_updater.sh is already running with PID $pid, killing"
+        kill $pid
+    fi
+done
+
+# Add an artificial sleep to wait for the IPC handler to be ready to process requests
+sleep 0.5
+
+SETSTATUS="duskc --ignore-reply run_command setstatus"
+
+secs=0
+while true; do
+
+    $SETSTATUS 0 "$(date '+%d %B, %A %I:%M:%S %P' | tr '[:upper:]' '[:lower:]')" &
+
+    if [ $((secs % 30)) = 0 ]; then
+        $SETSTATUS 1 "$(dwmblocks_systemstats)" &
+    fi
+
+    ((secs+=1))
+    sleep 1
+done
